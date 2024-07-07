4b9467942aa047f46dacdcf9bea02a5c1221d60d
Agastya Hukoo <agastyahukoo@gmail.com>
2024-07-07 20:20:06 +0530

    Classes and Objects

---
+++ Classes and Objects/q34.py
@@ -0,0 +1,3 @@
+p = float(input("Enter the principal amount: "))
+t = float(input("Enter the amount of time in years: "))
+print("The simple interest is: ",(p*t*6)/100)
---
+++ Classes and Objects/q35.py
@@ -0,0 +1,3 @@
+r = float(input("Enter the radius of the cone: "))
+h = float(input("Enter the height of the cone: "))
+print("The volume of the cone is ",(1/3)*3.141*r*r*h)
