849232d52ea16472fe4e04546e15afe90a4abdc3
Agastya Hukoo <agastyahukoo@gmail.com>
2024-09-17 15:16:20 +0530

    Conditional Statements

--- Conditional Statements/q54.py
+++ Conditional Statements/q54.py
@@ -1,6 +1,6 @@
 a = (input("Enter two characters: "))
 b = (input())
 if ord(a) > ord(b):
-    print(f"{a} is the biggest number.")
+    print(f"{a} is the biggest character.")
 else:
-    print(f"{b} is the biggest number.")  
+    print(f"{b} is the biggest character.")  
---
+++ Conditional Statements/q55.py
@@ -0,0 +1,7 @@
+n = int(input("Enter a number: "))
+if n > 0:
+    print(f"{n} is a positive number.")
+elif n < 0:
+    print(f"{n} is a negative number.")
+else:
+    print(f"{n} is zero.")
